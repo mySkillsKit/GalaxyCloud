962570a93e04c6e19cecce9c46d7a546f383baa6
Anatolii Vasilev <avas2012@gmail.com>
2022-10-17 09:46:57 +0300

    first commit

---
+++ src/main/java/ru/netology/galaxycloud/GalaxyCloudApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.galaxycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxyCloudApplication.class, args);
+    }
+
+}
